25380173ea6c0117af056d772a6b93e63bca6998
johnsonjw48 <johnsonjw48@gmail.com>
2024-09-27 01:10:43 +0200

    first commit

---
+++ src/Controller/BookController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\BookRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class BookController extends AbstractController
+{
+    #[Route('/api/books', name: 'app_book')]
+    public function getAllBooks(BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $bookList = $bookRepository->findAll();
+        $jsonBookList = $serializer->serialize($bookList, 'json');
+        return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
+    }
+}
